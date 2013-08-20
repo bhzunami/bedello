22546365a20b5daf09a6890649fea98f042eb498
Nicolas Mauchle <nmauchle@gmail.com>
2013-08-20 16:43:06 +0200

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bedello::Application.config.secret_key_base = 'e73478fca7fa61468509c0ef3f41573db3280da3c60d42219d8132936a16fe2333117ad1a63f25c04f4a4ded744933812867cd27c82139f8eaf8cdf3d2f9122c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bedello::Application.config.session_store :cookie_store, key: '_bedello_session'
