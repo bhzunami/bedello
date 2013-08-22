5200dff139c7befd789525997ecd1bd564c99b54
Nicolas Mauchle <nmauchle@gmail.com>
2013-08-22 14:59:19 +0200

    Add Design for index and show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,34 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                 :integer          not null, primary key
+#  title              :string(255)
+#  description        :text
+#  productNr          :integer
+#  price              :decimal(, )
+#  promotionPrice     :decimal(, )
+#  promotionStartDate :datetime
+#  promotionEndDate   :datetime
+#  image              :string(255)
+#  isActivate         :boolean
+#  inStock            :integer
+#  saleStartDate      :datetime
+#  salesEndDate       :datetime
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class Product < ActiveRecord::Base
+
+
+	validates :title, :productNr, uniqueness: true
+	validates :productNr, numericality: { greater_than_or_equal_to: 0 }
+	validates :inStock, numericality: true
+	validates :title, :description, :productNr, :price, :inStock, :saleStartDate, :salesEndDate, presence: true
+
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :promotionPrice, numericality: { greater_than_or_equal_to: 0.00 }
+
+
 end
