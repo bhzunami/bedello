6460d44512ba9ed11da87be4cfdff557d2f60089
Nicolas Mauchle <nmauchle@gmail.com>
2014-02-10 17:19:26 +0100

    Remove the closeStore date (include in create openDate)

--- db/migrate/20140210144557_change_store_is_open.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStoreIsOpen < ActiveRecord::Migration
-  def change
-  	change_column :website_settings, :webstoreOpen, :date
-  	add_column :website_settings, :webstoreClose, :date
-  end
-end
