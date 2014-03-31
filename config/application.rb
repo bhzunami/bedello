ca3f9dbb8e6341bd5f7ee585ebff0dcf796dac6f
Nicolas Mauchle <nmauchle@gmail.com>
2014-03-31 15:14:14 +0200

    Add de-CH

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Bedello
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = 'de-CH'
+    config.encoding = "utf-8"
     #3config.i18n.enforce_available_locales = false
     ##I18n.enforce_available_locales = true
     # 
