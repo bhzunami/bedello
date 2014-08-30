69bf6757f56b3caf60c43e86978e72152851587d
Nicolas Mauchle <nmauchle@gmail.com>
2014-08-30 12:43:47 +0200

    Add https to paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Bedello::Application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_protocol => :https
+  :s3_protocol => :https,
   :s3_credentials => {
     :bucket => ENV['AMAZON_S3_BUCKET_NAME'],
     :access_key_id => ENV['AMAZON_AWS_ACCESS_KEY_ID'],
